8398926bb89e7383fae15ef5e22cb79e14eb2e24
shruti chawla <114687114+Shrutichawla786@users.noreply.github.com>
2024-05-08 00:05:06 +0530

    Day2

--- Day2.cpp
+++ Day2.cpp
@@ -1,18 +1,28 @@
-Q.Find missing in second array
-code->vector<int> findMissing(int a[], int b[], int n, int m) 
-	{ 
-	    // Your code goes here
-	    vector<int>ans;
-	    unordered_map<int , int>mp;
-	    for(int i=0;i<m;i++){
-	        mp[b[i]]++;
-	    }
-	    for(int i=0;i<n;i++){
-	        if(mp[a[i]]==0){
-	            ans.push_back(a[i]);
-	        }
-	    }
-	    return ans;
-	}
-Expected Time Complexity: O(n+m).
-Expected Auxiliary Space: O(m).
+Q.Root to Leaf Paths
+code->
+
+class Solution {
+  public:
+  void solve(Node* root,vector<vector<int>>&path,vector<int>currentpath){
+      if(root==NULL){
+          return ;
+      }
+      currentpath.push_back(root->data);
+      if(root->left==NULL &&root->right==NULL){
+          path.push_back(currentpath);
+      }
+      solve(root->left , path , currentpath);
+      solve(root->right , path , currentpath);
+      currentpath.pop_back();
+  }
+    vector<vector<int>> Paths(Node* root) {
+        // code here
+        vector<vector<int>>path;
+        vector<int>currentpath;
+        solve(root , path , currentpath);
+        return path;
+    }
+};
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(height of the tree)
+
