8499319c586262b8a1e48fdb80ee27038cdeb54f
shruti chawla <114687114+Shrutichawla786@users.noreply.github.com>
2024-05-09 19:34:26 +0530

    Day3

--- Day3.cpp
+++ Day3.cpp
@@ -1,20 +1,18 @@
-Q.Union of Two Sorted Arrays
-code--> vector<int> findUnion(int arr1[], int arr2[], int n, int m)
-    {
-        //Your code here
-        //return vector with correct order of elements
-        set<int>s;
-        for(int i=0;i<n;i++){
-            s.insert(arr1[i]);
-        }
-        for(int j=0;j<m;j++){
-            s.insert(arr2[j]);
-        }
-        vector<int>ans;
-        for(auto i:s){
-            ans.push_back(i);
-        }
-        return ans;
+Q.Divisor Game
+code-->
+  bool solve(int n){
+      if(n<=1){
+          return false;
+      }
+      for(int i=1;i<n;i++){
+          if(n%i==0){
+             return !solve(n-i);
+          }
+      }
+      return false;
+  }
+    bool divisorGame(int n) {
+        // code here
+        return solve(n);
+    
     }
-Expected Time Complexity: O(n+m).
-Expected Auxiliary Space: O(n+m).
