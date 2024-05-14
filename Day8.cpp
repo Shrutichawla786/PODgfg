aa02349938e7a664fdf84486cfa3299cfd4677b2
shruti chawla <114687114+Shrutichawla786@users.noreply.github.com>
2024-05-14 16:38:18 +0530

    Day8

--- Day8.cpp
+++ Day8.cpp
@@ -1,30 +1,44 @@
-Q.Delete Middle of Linked List
-code->class Solution{
-    public:
-    Node* deleteMid(Node* head)
-{
-    if(head==NULL || head->next==NULL){
-        return NULL;
+Q.Path With Minimum Effort
+code->
+    int MinimumEffort(int rows, int columns, vector<vector<int>> &heights) {
+               priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+        vector<vector<int>> dis(heights.size(),vector<int>(heights[0].size(),1e9));
+         
+        int n=heights.size();
+        int m=heights[0].size();
+         
+        pq.push({0,{0,0}});
+        dis[0][0]=0;
+         
+        int x[4]={1,-1,0,0};
+        int y[4]={0,0,1,-1};
+        
+        while(!pq.empty())
+        {
+            auto it=pq.top();
+            pq.pop();
+            
+            int d=it.first;
+            int r=it.second.first;
+            int c=it.second.second;
+            
+            if(r==n-1 and c==m-1)
+            return d;
+            
+            for(int i=0;i<4;i++)
+            {
+                int ri=r+x[i];
+                int ci=c+y[i];
+                
+                if(ri>=0 and ri<n and ci>=0 and ci<m)
+                {
+                    int maxi=max(abs(heights[r][c]-heights[ri][ci]),d);
+                    if(dis[ri][ci]>maxi)
+                    {
+                        dis[ri][ci]=maxi;
+                        pq.push({maxi,{ri,ci}});
+                    }
+                }
+            }
+        }
     }
-        Node * temp = head;
-        int count=1;
-        while(temp!=NULL){
-            count++;
-            temp=temp->next;
-        }
-        Node * temp2= head;
-        int i;
-        if(count%2==0){
-            i=2;
-        }
-        else{
-            i=1;
-        }
-        while(i<count/2){
-            temp2=temp2->next;
-            i++;
-        }
-        temp2->next= temp2->next->next;
-        return head;
-    }
-};
