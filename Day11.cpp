f8ee5238fbcf8f6ec6b05b559a8b09ed701212e6
shruti chawla <114687114+Shrutichawla786@users.noreply.github.com>
2024-05-17 16:54:28 +0530

    Day11

--- Day11.cpp
+++ Day11.cpp
@@ -1,70 +1,24 @@
-Q.Serialize and deserialize a binary tree
-code-->class Solution
-{
-    public:
-    //Function to serialize a tree and return a list containing nodes of tree.
-    vector<int> serialize(Node *root) 
-    {
-        //Your code here
-        vector<int>v;
-        queue<Node*>q;
-        q.push(root);
-        while(!q.empty()){
-            Node * node = q.front();
-            q.pop();
-            if(node){
-                v.push_back(node->data);
+Q.Find Pair Given Difference
+code-->
+class Solution {
+  public:
+    int findPair(int n, int x, vector<int> &arr) {
+        // code here
+        unordered_map<int , int>mp;
+        for(int i=0;i<n;i++){
+            mp[arr[i]]++;
+            if(x==0 && mp[arr[i]]==2){
+                return 1;
             }
-            else{
-                v.push_back(-1);
-            }
-            if(node){
-                q.push(node->left);
-                q.push(node->right);
-            }
-            
         }
-        return v;
+        if(x==0){
+            return -1;
+        }
+        for(auto it:mp){
+            if(mp.find(x+it.first)!= mp.end()){
+                return true;
+            }
+        }
+        return -1;
     }
-    
-    //Function to deserialize a list and construct the tree.
-    Node * deSerialize(vector<int> &A)
-    {
-       //Your code here
-       Node * root= new Node(A[0]);
-       queue<Node*>q;
-       q.push(root);
-       int i=1;
-       
-       while(!q.empty() and i<A.size()){
-           Node * node = q.front();
-           q.pop();
-           bool FR=0;
-           bool FL=0;
-           if(A[i]==-1){
-               node->left = NULL;
-               FL=1;
-           }
-           if(A[i+1]==-1){
-               node->right = NULL;
-               FR=1;
-           }
-       
-       if(!FL){
-           Node * newnode= new Node(A[i]);
-           node->left = newnode;
-           q.push(newnode);
-       }
-       if(!FR){
-           Node * newnode = new Node(A[i+1]);
-           node->right =newnode;
-           q.push(newnode);
-       }
-       i+=2;
-       }
-       return root;
-    }
-
 };
-Expected Time Complexity: O(Number of nodes).
-Expected Auxiliary Space: O(Number of nodes).
