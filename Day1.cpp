41656a7a473ab204c532cb69306dc3c3553b7e0c
shruti chawla <114687114+Shrutichawla786@users.noreply.github.com>
2024-05-26 19:29:40 +0530

    Day1

--- Day1.cpp
+++ Day1.cpp
@@ -1,31 +1,33 @@
-Reverse Level Order Traversal
-code-->void level(Node * root , vector<int>&ans){
-    if(root==NULL){
-        return;
-    }
-    queue<Node*>q;
-    q.push(root);
-    while(!q.empty()){
-        Node * front= q.front();
-        q.pop();
-        ans.push_back(front->data);
-        if(front->right){
-            q.push(front->right);
-        }
-        if(front->left){
-            q.push(front->left);
-        }
+Minimum Cost To Make Two Strings Identical
 
+ int dp[1001][1001] ;
+  int solve(string &x , string &y , int costx , int costy , int i , int j){
+      if(i>=x.length() && j>=y.length()){
+          return 0;
+      }
+      
+      if(i>=x.length()){
+          return costy*(y.length()-j);
+      }
+      if(j>=y.length()){
+          return costx*(x.length()-i);
+      }
+      if(dp[i][j] != -1)
+           return dp[i][j] ;
+           
+      if(x[i]==y[j]){
+          return 0+solve(x , y , costx , costy , i+1 , j+1);
+      }
+      else{
+          int op1= costx+ solve(x , y , costx , costy ,i+1 , j);
+          int op2= costy+solve(x ,y , costx , costy , i , j+1);
+          return dp[i][j] =min(op1 , op2);
+      }
+      
+  }
+    int findMinCost(string x, string y, int costX, int costY) {
+        // Your code goes here
+        
+        memset(dp,-1,sizeof(dp));
+        return solve(x , y , costX , costY , 0,0);
     }
-    
-}
-vector<int> reverseLevelOrder(Node *root)
-{
-    // code here
-    vector<int>ans;
-    level(root , ans);
-    reverse(ans.begin() , ans.end());
-    return ans;
-    
-    
-}
